ca7bc0cc7aa7ab320bd42ebe2ca7d487371c0c6a
Starinin Andrey <starinin-andrey@ya.ru>
2025-03-07 20:44:53 +0300

    Добавление класса Article

---
+++ KnowledgeBase.Core/Article.cs
@@ -0,0 +1,12 @@
+namespace KnowledgeBase.Core;
+
+public class Article
+{
+    public Guid Id { get; init; }
+    public required string Title { get; set; }
+    public List<string> Tags { get; set; } = [];
+    public required string Content { get; set; }
+    public DateTime DateOfCreation { get; init; }
+    public DateTime DateOfLastUpdate { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
