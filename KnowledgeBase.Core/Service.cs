36057355093aa75fc3b22842aad50661e392e0f3
Starinin Andrey <starinin-andrey@ya.ru>
2025-03-17 20:52:36 +0300

    Создание класса Service для реализации интерфейса IService

---
+++ KnowledgeBase.Core/Service.cs
@@ -0,0 +1,51 @@
+﻿using KnowledgeBase.Model;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace KnowledgeBase.Core;
+
+public record ConnectConfig(string ConnectionString, string DatabaseName, string CollectionName);
+
+public class Service : IService
+{
+    private readonly ConnectConfig _config;
+    
+    private string ConnectionString => _config.ConnectionString;
+    private string DatabaseName => _config.DatabaseName;
+    private string CollectionName => _config.CollectionName;
+    
+    private readonly IMongoCollection<Article> _collection;
+    
+    public Service(ConnectConfig config)
+    {
+        _config = config;
+
+        var client = new MongoClient(ConnectionString);
+        var db = client.GetDatabase(DatabaseName);
+        _collection = db.GetCollection<Article>(CollectionName);
+    }
+    
+    public void Create(Article article) => 
+        _collection.InsertOne(article);
+
+    public void Update(Article article) => 
+        _collection.ReplaceOne(a => a.Id == article.Id, article);
+
+    public void Delete(Guid id) => 
+        _collection.DeleteOne(a => a.Id == id);
+
+    public IEnumerable<Article> GetAll() => 
+        _collection
+            .Find(new BsonDocument())
+            .ToList();
+
+    public Article? GetBy(Guid id) => 
+        _collection
+            .Find(a => a.Id == id)
+            .SingleOrDefault();
+
+    public IEnumerable<Article>? GetBy(IEnumerable<string> tags) => 
+        _collection
+            .Find(a => a.Tags.Any(tags.Contains))
+            .ToList();
+}
