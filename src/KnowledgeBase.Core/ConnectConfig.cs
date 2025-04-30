c6056475b57cc905df7b3d27565b95305efac64d
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-30 21:05:52 +0300

    Выделение ConnectConfig в отдельный файл

---
+++ src/KnowledgeBase.Core/ConnectConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace KnowledgeBase.Core;
+
+/// <summary>
+/// Конфигурация подключения к базе данных
+/// </summary>
+/// <param name="ConnectionString">Строка подключения</param>
+/// <param name="DatabaseName">Имя базы данных</param>
+/// <param name="CollectionName">Имя коллекции</param>
+public record ConnectConfig(string ConnectionString, string DatabaseName, string CollectionName);
